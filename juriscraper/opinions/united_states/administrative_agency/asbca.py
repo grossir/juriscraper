e64f1abb5d1107e4f54b0c9648edbc7b0719d385
Gianfranco Rossi <gianfranco.rossi.r@gmail.com>
2024-01-24 14:29:15 -0500

    fix(asbca): now parses judges
    Also, refactored the code and added comments

--- juriscraper/opinions/united_states/administrative_agency/asbca.py
+++ juriscraper/opinions/united_states/administrative_agency/asbca.py
@@ -6,10 +6,12 @@ Reviewer: mlr
 History:
     2014-09-11: Created by Jon Andersen
     2016-03-17: Website and phone are dead. Scraper disabled in __init__.py.
+    2024-01-24: Add parsing of judges
 """
 
 from datetime import datetime
 
+from juriscraper.lib.string_utils import clean_string
 from juriscraper.OpinionSiteLinear import OpinionSiteLinear
 
 
@@ -17,21 +19,33 @@ class Site(OpinionSiteLinear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.court_id = self.__module__
-        self.url = (
-            "http://www.asbca.mil/Decisions/decisions%d.html"
-            % datetime.today().year
-        )
+        self.url = f"http://www.asbca.mil/Decisions/decisions{datetime.today().year}.html"
         self.status = "Published"
 
-    def _process_html(self):
+    def _process_html(self) -> None:
+        """Parse HTML into case objects
+
+        :return: None
+        """
         for row in self.html.xpath(".//tr")[1:]:
-            col1, col2, col3, col4 = row.xpath(".//td")
-            date = col1.text_content().strip()
-            if not col2.text_content().strip():
+            date, docket, *_, judge = (
+                clean_string(cell) for cell in row.xpath("td/text()")
+            )
+            if not docket:
+                # Row that separates months
                 continue
-            name = col3.text_content().strip()
-            docket = col2.text_content().strip()
-            url = col3.xpath(".//a/@href")[0]
+
+            url = row.xpath(".//a/@href")[0]
+            # Part of the name may be inside <a>, part inside <span> or
+            # floating as <td>'s text
+            name = clean_string(row.xpath("string(.//td[3])"))
+
             self.cases.append(
-                {"date": date, "name": name, "url": url, "docket": docket}
+                {
+                    "date": date,
+                    "name": name,
+                    "url": url,
+                    "docket": docket,
+                    "judge": judge,
+                }
             )
